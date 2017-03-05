b7b6706df35394e1ef96ef14e90febf2d1d960c0
martinevsky <max@martinevsky.ru>
2017-03-05 13:49:49 +0300

    Initial layout version

--- source/app.d
+++ source/app.d
@@ -1,11 +1,23 @@
+import std.functional;
+import std.exception;
+
+import vibe.http.router;
 import vibe.http.server;
+import vibe.http.fileserver;
 
 private void main()
 {
+	auto router = new URLRouter;
+	router.get("/", &showHome);
+	router.get("*", serveStaticFiles("public"));
+
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP (settings, &hello);
+	settings.errorPageHandler = toDelegate(&showError);
+	settings.accessLogToConsole = true;
+
+	listenHTTP (settings, router);
 
 	import vibe.core.core;
 
@@ -19,7 +31,12 @@ private void main()
 	runApplication();
 }
 
-private void hello (HTTPServerRequest req, HTTPServerResponse res)
+private void showError (HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error)
 {
-	res.writeBody ("Hello, World!");
+	res.render!("error.dt", req, error);
+}
+
+private void showHome (HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.render!("home.dt", req);
 }
